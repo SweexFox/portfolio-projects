5588ea2390fe76a8cfd0b0a37cf5e5bc1d72b5ab
Kirill Kuz'minov <happyfurbolg@yandex.ru>
2023-08-31 14:45:03 +0300

    Add files via upload

---
+++ sql-projects/stackoverflow/stackoverflow_part_1.sql
@@ -0,0 +1,164 @@
+-- 1. Количество вопросов, которые набрали больше 300 очков или как минимум 100 раз были добавлены в «Закладки»
+SELECT COUNT(p.id)
+FROM stackoverflow.posts AS p
+INNER JOIN stackoverflow.post_types AS pt ON p.post_type_id = pt.id
+WHERE type = 'Question'
+ AND (score > 300
+   OR favorites_count >= 100);
+
+
+-- 2. Среднее количество заданных с 1 по 18 ноября 2008 года вопросов
+SELECT ROUND(AVG(questions)) AS questions_per_day
+FROM (SELECT creation_date::date,
+	         COUNT(title) AS questions
+      FROM stackoverflow.posts AS p
+      INNER JOIN stackoverflow.post_types AS pt ON p.post_type_id = pt.id
+      WHERE CAST(creation_date AS date) BETWEEN '2008-11-01' AND '2008-11-18'
+        AND type = 'Question'
+      GROUP BY creation_date::date) AS subquery;
+
+
+-- 3. Количество уникальных пользователей, которые в день регистрации получили значки за достижения
+SELECT COUNT(DISTINCT user_id) AS unique_users
+FROM (SELECT u.id AS user_id,
+	         name,
+	         u.creation_date::date AS register_dt,
+	         b.creation_date::date AS badge_dt
+      FROM stackoverflow.users AS u
+      INNER JOIN stackoverflow.badges AS b ON u.id = b.user_id
+      WHERE u.creation_date::date = b.creation_date::date) AS subquery
+
+
+-- 4. Количество уникальных постов пользователя с именем Joel Coehoorn, которые получили хотя бы один голос
+SELECT COUNT(DISTINCT p.id)
+FROM stackoverflow.posts AS p
+INNER JOIN stackoverflow.votes AS v ON p.id = v.post_id
+WHERE p.user_id IN (SELECT id
+                    FROM stackoverflow.users
+                    WHERE display_name = 'Joel Coehoorn')
+
+
+-- 5. Все поля таблицы vote_types. К ней добавлено поле rank, в которое войдут номера записей в обратном порядке
+SELECT *,
+	   ROW_NUMBER() OVER (ORDER BY id DESC) AS rank
+FROM stackoverflow.vote_types
+ORDER BY id;
+
+
+-- 6. 10 пользователей, которые поставили больше всего голосов типа Close
+SELECT user_id,
+	   COUNT(name) AS close_votes
+FROM stackoverflow.votes AS v
+INNER JOIN stackoverflow.vote_types AS vt ON v.vote_type_id = vt.id
+WHERE name = 'Close'
+GROUP BY user_id
+ORDER BY close_votes DESC,
+		 user_id DESC
+LIMIT 10;
+
+
+-- 7. 10 пользователей по количеству значков, полученных в период с 15 ноября по 15 декабря 2008 года включительно
+SELECT *,
+	   DENSE_RANK() OVER (ORDER BY badges DESC) AS rank
+FROM (SELECT user_id,
+	         COUNT(id) AS badges
+      FROM stackoverflow.badges
+      WHERE creation_date::date BETWEEN '2008-11-15' AND '2008-12-15'
+      GROUP BY user_id
+      ORDER BY badges DESC,
+		       user_id
+      LIMIT 10) AS badges;
+
+
+-- 8. Таблица из следующих полей:
+-- заголовок поста;
+-- идентификатор пользователя;
+-- число очков поста;
+-- среднее число очков пользователя за пост, округлённое до целого числа.
+-- Не учитываются посты без заголовка, а также те, что набрали ноль очков
+SELECT title,
+	   user_id,
+	   score,
+	   ROUND(AVG(score) OVER (PARTITION BY user_id)) AS avg_score
+FROM stackoverflow.posts
+WHERE title IS NOT NULL
+  AND score <> 0;
+
+
+-- 9. Заголовки постов, которые были написаны пользователями, получившими более 1000 значков. Посты без заголовков попадут в список
+SELECT title
+FROM stackoverflow.posts
+WHERE user_id IN (SELECT user_id
+                  FROM (SELECT user_id,
+	                           COUNT(name) AS badges
+                        FROM stackoverflow.badges
+                        GROUP BY user_id
+                        HAVING COUNT(name) > 1000
+                        ORDER BY badges DESC) top_user)
+AND title IS NOT NULL;
+
+
+-- 10. Пользователи разделены на три группы в зависимости от кол-ва просмотров их профилей:
+-- пользователям с числом просмотров больше либо равным 350 присвойте группу 1;
+-- пользователям с числом просмотров меньше 350, но больше либо равно 100 — группу 2;
+-- пользователям с числом просмотров меньше 100 — группу 3.
+-- В таблицу попадут пользователи из США без нулевых просмотров профилей
+SELECT id,
+       views,
+       CASE
+       	   WHEN views >= 350 THEN 1
+       	   WHEN views < 350 AND views >= 100 THEN 2
+       	   WHEN views < 100 THEN 3
+       END AS group
+FROM stackoverflow.users
+WHERE views <> 0
+  AND location LIKE '%United States%';
+
+
+-- 11. Из предыдущего запроса отображены лидеры каждой группы — пользователи, которые набрали максимальное число просмотров в своей группе 
+WITH
+t1 AS (SELECT id,
+	          groups,
+	          views,
+	          MAX(views) OVER (PARTITION BY groups) AS max_views
+       FROM (SELECT id,
+                    views,
+                    CASE
+       	                WHEN views >= 350 THEN 1
+       	                WHEN views < 350 AND views >= 100 THEN 2
+       	                WHEN views < 100 THEN 3
+                    END AS groups
+             FROM stackoverflow.users
+             WHERE views <> 0
+             AND location LIKE '%United States%') AS subquery)
+SELECT id,
+	   GROUPS,
+	   views
+FROM t1
+WHERE views = max_views
+ORDER BY views DESC,
+		 id
+
+
+-- 12. Ежедневный прирост новых пользователей в ноябре 2008 года
+SELECT *,
+       SUM(users) OVER (ORDER BY november_day) AS cum_sum
+FROM (SELECT EXTRACT(DAY FROM creation_date::date) AS november_day,
+	         COUNT(id) AS users
+      FROM stackoverflow.users
+      WHERE EXTRACT(YEAR FROM creation_date) = 2008
+        AND EXTRACT(MONTH FROM creation_date) = 11
+      GROUP BY 1) AS subquery;
+
+
+-- 13. Интервал между регистрацией и временем создания первого поста для каждого пользователя, который написал хотя бы один пост
+SELECT user_id,
+	   first_post_date - reg_date AS time_diff
+FROM (SELECT user_id,
+	         u.creation_date AS reg_date,
+	         p.creation_date AS post_date,
+	         FIRST_VALUE(p.creation_date) OVER(PARTITION BY user_id ORDER BY p.creation_date) AS first_post_date
+      FROM stackoverflow.posts AS p
+      INNER JOIN stackoverflow.users AS u ON p.user_id = u.id
+      ORDER BY 1) AS subquery
+WHERE first_post_date = post_date;
---
+++ sql-projects/stackoverflow/stackoverflow_part_2.sql
@@ -0,0 +1,123 @@
+-- 1.
+-- Общая сумма просмотров постов за каждый месяц 2008 года
+SELECT DATE_TRUNC('month', creation_date)::date AS month,
+	   SUM(views_count) AS views
+FROM stackoverflow.posts
+WHERE EXTRACT(YEAR FROM creation_date) = 2008
+GROUP BY month
+ORDER BY views DESC;
+-- данные отличаются, повышенная активность видна в сентябре и октябре. Возможно, это связано с началом учебного года. Просмотры в июле могут говорить о неполноте данных
+
+
+-- 2.
+-- Имена самых активных пользователей, которые в первый месяц после регистрации (включая день регистрации) дали больше 100 ответов.
+-- Не учитываются вопросы, которые задавали пользователи. 
+-- Для каждого имени пользователя выведено количество уникальных значений user_id
+SELECT display_name,
+	   COUNT(DISTINCT user_id) AS user_ids
+FROM stackoverflow.posts AS p
+INNER JOIN stackoverflow.users AS u ON p.user_id = u.id
+WHERE post_type_id IN (SELECT id
+					   FROM stackoverflow.post_types
+					   WHERE type = 'Answer')
+  AND p.creation_date <= u.creation_date + INTERVAL '1 month'
+GROUP BY 1
+HAVING COUNT(post_type_id) > 100
+ORDER BY display_name;
+-- Видим, что одно и то же имя пользователя может принадлежать многим идентификаторам.
+-- Следовательно, данные лучше анализировать имеено по id, а не по отображаемым именам.
+
+
+-- 3.
+-- Количество постов за 2008 год по месяцам от пользователей, которые зарегистрировались в сентябре 2008 года и сделали хотя бы один пост в декабре того же года
+SELECT DATE_TRUNC('month', creation_date)::date AS month,
+	   COUNT(id)	   
+FROM stackoverflow.posts
+WHERE user_id IN (SELECT user_id
+                  FROM (SELECT user_id,
+	                           u.creation_date AS reg_date,
+	                           p.creation_date AS post_date
+                        FROM stackoverflow.users AS u
+                        INNER JOIN stackoverflow.posts AS p ON u.id = p.user_id
+                        WHERE u.creation_date::date BETWEEN '2008-09-01' AND '2008-09-30'
+                        AND p.creation_date::date BETWEEN '2008-12-01' AND '2008-12-31'
+                        ORDER BY 3) AS subquery)
+GROUP BY month
+ORDER BY month DESC;
+-- есть аномальные значения: почему-то у пользователей, зарегистрировавшихся в сентябре, есть активность в августе. Возможна ошибка в данных
+
+
+-- 4.
+-- Несколько полей:
+-- идентификатор пользователя, который написал пост;
+-- дата создания поста;
+-- количество просмотров у текущего поста;
+-- сумму просмотров постов автора с накоплением
+SELECT user_id,
+	   creation_date,
+	   views_count,
+	   SUM(views_count) OVER (PARTITION BY user_id ORDER BY creation_date) AS cum_sum
+FROM stackoverflow.posts;
+
+
+-- 5.
+-- Сколько в среднем дней в период с 1 по 7 декабря 2008 года включительно пользователи взаимодействовали с платформой
+WITH
+days_per_user AS (SELECT user_id,
+	                     COUNT(post_date) AS active_days
+                  FROM (SELECT user_id,
+	                           creation_date::date AS post_date
+                        FROM stackoverflow.posts
+                        WHERE creation_date::date BETWEEN '2008-12-01' AND '2008-12-07'
+                        GROUP BY user_id,
+		                         creation_date::date
+                        ORDER BY user_id,
+		                         creation_date::date) AS subquery
+                  GROUP BY user_id)
+SELECT ROUND(AVG(active_days)) AS av_days
+FROM days_per_user;
+-- следовательно, в период с 1 по 7 декабря 2008 года пользователи взаимодействовали с платформой в среднем 2 дня
+
+
+-- 6.
+-- На сколько процентов менялось количество постов ежемесячно с 1 сентября по 31 декабря 2008 года? 
+-- Таблица со следующими полями:
+-- номер месяца;
+-- количество постов за месяц;
+-- процент, который показывает, насколько изменилось количество постов в текущем месяце по сравнению с предыдущим
+WITH
+changes AS (SELECT *,
+	               LAG(posts) OVER () AS last_month_posts
+            FROM (SELECT EXTRACT(MONTH FROM creation_date) AS month_number,
+	                     COUNT(id) AS posts
+                  FROM stackoverflow.posts
+                  WHERE creation_date BETWEEN '2008-09-01' AND '2008-12-31'
+                  GROUP BY month_number) AS subquery)
+SELECT month_number,
+	   posts,
+	   ROUND((posts::numeric / last_month_posts - 1) * 100, 2) AS percent_changes
+FROM changes;
+
+
+-- 7. 
+-- Данные активности пользователя, который опубликовал больше всего постов за всё время. 
+-- Выведены данные за октябрь 2008 года в таком виде:
+-- номер недели;
+-- дата и время последнего поста, опубликованного на этой неделе
+WITH
+usr_oct_actions AS (SELECT EXTRACT(WEEK FROM creation_date) AS week,
+	                       creation_date,
+	                       MAX(creation_date) OVER (PARTITION BY EXTRACT(WEEK FROM creation_date)) AS last_post_week
+                    FROM stackoverflow.posts
+                    WHERE user_id IN (SELECT user_id
+                                      FROM (SELECT user_id,
+	                                               COUNT(id) AS posts
+                                            FROM stackoverflow.posts
+                                            GROUP BY user_id
+                                            ORDER BY posts DESC
+                                            LIMIT 1) AS subquery)
+                    AND creation_date::date BETWEEN '2008-10-01' AND '2008-10-31')
+SELECT DISTINCT week,
+	   last_post_week
+FROM usr_oct_actions
+ORDER BY week;
